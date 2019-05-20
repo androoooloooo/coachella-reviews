024cbf84fcd246f07ad1b2a92f973aae97bfe70e
loooo <liuandrew878@gmail.com>
2019-05-19 23:31:51 -0700

    added initial MVC for review list

--- mysite/reviews/models.py
+++ mysite/reviews/models.py
@@ -3,9 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Review(models.Models):
-    Artist = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Review(models.Model):
+    artist = models.CharField(max_length=100)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
     review_text = models.CharField(max_length=500)
     date = models.DateField()
     coachellaYear = models.DateField()
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.review_text
--- mysite/reviews/views.py
+++ mysite/reviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
+from .models import Review
 # Create your views here.
+
+
+class ReviewList(generic.ListView):
+    model = Review
